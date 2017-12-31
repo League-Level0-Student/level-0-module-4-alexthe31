fa41eb5b3d0e657394a0d6c1e9ee2e0ad5a9c3cb
alexthe31 <league@wts10.attlocal.net>
2017-12-31 15:58:45 -0800

    changecalc

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -14,17 +14,20 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		String nickels = JOptionPane.showInputDialog("are you broke n");
+		
 		// Convert their answer to an int using Integer.parseInt()
-
+		int numn = Integer.parseInt(nickels);
 		// Ask the user how many dimes they have, and convert their answer
-
+		String dimes = JOptionPane.showInputDialog("are you broke d");
+		int numd = Integer.parseInt(dimes);
 		// Ask the user how many quarters they have, and convert their answer
-
+		String quarters = JOptionPane.showInputDialog("are you broke q");
+		int numq = Integer.parseInt(quarters);
 		// Calculate how much money the user has and save it in a double variable 
-
+int sum = numn*5 + numd*10 + numq*25;
 		// Tell the user how much money they have
-
+System.out.println(sum);
 	}
 }
 
